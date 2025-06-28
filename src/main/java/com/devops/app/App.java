961f974451b46ea6efa36799eca95e15954a29d1
root <root@ip-172-31-35-253.ap-south-1.compute.internal>
2025-06-28 16:48:16 +0000

    Initial commit - Java Spring Boot

---
+++ src/main/java/com/devops/app/App.java
@@ -0,0 +1,20 @@
+package com.devops.app;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@RestController
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello, DevOps!";
+    }
+}
+
